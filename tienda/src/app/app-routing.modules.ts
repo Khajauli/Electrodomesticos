76d823d2b4459d3615bdfcdbbf25c3d423f5edb3
Ivanna-Daniela <ivanna.cevallos@epn.edu.ec>
2023-03-10 21:09:13 -0500

    Footer y despliegue de productos

--- tienda/src/app/app-routing.modules.ts
+++ tienda/src/app/app-routing.modules.ts
@@ -4,8 +4,9 @@ import { ContactoComponent } from './components/contacto/contacto.component';
 import { CreateElectroComponent } from './components/create-electro/create-electro.component';
 import { DetalleElectroComponent } from './components/detalle-electro/detalle-electro.component';
 import { EditarElectroComponent } from './components/editar-electro/editar-electro.component';
+import { ElectrodomesticosComponent } from './components/electrodomesticos/electrodomesticos.component';
 import { HomeComponent } from './components/home/home.component';
-import { ElectrodomesticoService } from './services/electrodomestico.service';
+
 
 const routes: Routes = [
     {path:'inicio',component:HomeComponent},
@@ -13,7 +14,7 @@ const routes: Routes = [
     {path:'contacto',component:ContactoComponent},
     {path:'detalleElectro/:id',component:DetalleElectroComponent},
     {path:'editarElectro/:id',component:EditarElectroComponent},
-    {path:'electrodomesticos',component:ElectrodomesticoService},
+    {path:'electrodomesticos',component:ElectrodomesticosComponent},
     {path:'**',component:HomeComponent},
   ];
   
--- tienda/src/app/components/electrodomesticos/electrodomesticos.component.ts
+++ tienda/src/app/components/electrodomesticos/electrodomesticos.component.ts
@@ -15,6 +15,7 @@ export class ElectrodomesticosComponent implements OnInit{
   public electros:Electrodomestico[];
   public nombre:string;
   public url:string;
+  public categorias: string[];
 
   constructor(
     private _electroService:ElectrodomesticoService
@@ -23,6 +24,7 @@ export class ElectrodomesticosComponent implements OnInit{
     this.electrodomesticos=[];
     this.electros=[];
     this.nombre='';
+    this.categorias = [];
   }
 
 
@@ -35,9 +37,11 @@ export class ElectrodomesticosComponent implements OnInit{
       if (response.electrodomesticos){
         this.electrodomesticos=response.electrodomesticos;
         console.log(this.electrodomesticos);
+        this.obtenerCategorias();
       }
     },
     error=>{
+      console.log("error1");
       console.log(<any>error);
     }
   );
@@ -55,5 +59,23 @@ export class ElectrodomesticosComponent implements OnInit{
         console.log(<any>error);
       }
     )
+  }
+  obtenerCategorias() {
+    let categorias: string[] = [];
+    let contador: number;
+
+    for (let categoria of this.electrodomesticos) {
+      contador = 0;
+      for (let producto of this.electrodomesticos) {
+        if (producto.estado === categoria.estado) {
+          contador++;
+        }
+      }
+      if (!categorias.includes(categoria.estado)) {
+        categorias.push(categoria.estado);
+        console.log(categoria.estado + ': ' + contador);
+      }
+    }
+    this.categorias = categorias; 
   }
 }
